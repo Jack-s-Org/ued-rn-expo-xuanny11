0920c460049a63ede29d364c9a22f6431a77c893
Jack Lim <wjie_lim@outlook.com>
2024-08-28 16:08:23 +0800

    Second test for students to pull from upstream

--- screens/AddScreen.js
+++ screens/AddScreen.js
@@ -1,6 +1,8 @@
 import { Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
+console.log("second test")
+
 const AddScreen = () => {
   return (
     <SafeAreaView>
