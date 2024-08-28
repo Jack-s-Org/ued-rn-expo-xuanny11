826e77fb2348e927967f4da7042a200461eadfc5
Jack Lim <wjie_lim@outlook.com>
2024-08-28 15:57:45 +0800

    Remove test for upstream pull

--- screens/AddScreen.js
+++ screens/AddScreen.js
@@ -1,8 +1,6 @@
 import { Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-console.log("test");
-
 const AddScreen = () => {
   return (
     <SafeAreaView>
